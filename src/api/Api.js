b79f0a53ce907b3ef1140e6d3acbee28c7e93004
Paulo Salvatore <salvatoregames@gmail.com>
2021-09-06 16:40:21 -0300

    feat(api): create file & implement GET

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,3 +1,15 @@
 export const Api = {
+    // Base URL
     baseUrl: "https://pokeapi.co/api/v2",
+
+    // Endpoints
+    readAllUrl: () => Api.baseUrl + "/pokemon",
+
+    // Funções de Requisição
+
+    // GET
+    buildApiGetRequest: url =>
+        fetch(url, {
+            method: "GET",
+        }),
 };
