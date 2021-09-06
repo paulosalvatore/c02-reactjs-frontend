67bf94efed76041408f05e34a8f34b46fb139148
Paulo Salvatore <salvatoregames@gmail.com>
2021-09-06 17:29:17 -0300

    feat(personagens): implement api

--- src/components/PersonagensList/PersonagensList.js
+++ src/components/PersonagensList/PersonagensList.js
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react";
+import { Api } from "../../api/Api";
+
 export function PersonagensList() {
-    return <div>PersonagensList</div>;
+    const [personagens, setPersonagens] = useState([]);
+
+    useEffect(() => {
+        const loadData = async () => {
+            const response = await Api.buildApiGetRequest(Api.readAllUrl());
+
+            const bodyResult = await response.json();
+
+            const results = bodyResult.results;
+
+            setPersonagens(results);
+        };
+
+        loadData();
+    }, []);
+
+    return (
+        <div>
+            {personagens.map((personagem, index) => (
+                <div key={"personagem_" + index}>{personagem.name}</div>
+            ))}
+        </div>
+    );
 }
