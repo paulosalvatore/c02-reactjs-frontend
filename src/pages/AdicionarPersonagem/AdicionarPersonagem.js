0aafd5d1a74e41f03235a54888614b695ed454f2
Paulo Salvatore <salvatoregames@gmail.com>
2021-09-10 16:34:31 -0300

    feat(personagem): build payload

--- src/pages/AdicionarPersonagem/AdicionarPersonagem.js
+++ src/pages/AdicionarPersonagem/AdicionarPersonagem.js
@@ -1,5 +1,61 @@
 import React from "react";
 
 export default function AdicionarPersonagem() {
-    return <div>AdicionarPersonagem</div>;
+    const handleSubmit = async event => {
+        event.preventDefault();
+
+        const nome = event.target.nome.value;
+        const imagemUrl = event.target.imagemUrl.value;
+
+        const payload = {
+            nome,
+            imagemUrl,
+        };
+
+        console.log(payload);
+
+        // TODO: Requisição POST para a API
+
+        // TODO: Redirecionar para página de visualização
+    };
+
+    return (
+        <div className="adicionar">
+            <form className="adicionar__form form" onSubmit={handleSubmit}>
+                <label htmlFor="nome" className="form__label">
+                    Nome:
+                </label>
+                <br />
+
+                <input
+                    type="text"
+                    id="nome"
+                    name="nome"
+                    className="form__input"
+                />
+
+                <br />
+
+                <label htmlFor="urlImagem" className="form__label">
+                    URL da Imagem:
+                </label>
+                <br />
+
+                <input
+                    type="text"
+                    id="urlImagem"
+                    name="urlImagem"
+                    className="form__input"
+                />
+
+                <br />
+
+                <input
+                    type="submit"
+                    value="Adicionar"
+                    className="form__submit button button--success"
+                />
+            </form>
+        </div>
+    );
 }
