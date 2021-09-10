cc9799ea879f920c8a0910753e7a98c7e0a5c197
Paulo Salvatore <salvatoregames@gmail.com>
2021-09-10 15:38:26 -0300

    fix(personagem): using relative path

--- src/pages/VisualizarPersonagem/VisualizarPersonagem.js
+++ src/pages/VisualizarPersonagem/VisualizarPersonagem.js
@@ -8,12 +8,12 @@ export default function VisualizarPersonagem() {
     return (
         <div className="personagem">
             <div className="personagem__buttons buttons">
-                <LinkButton to="/editar/1" className="buttons__button">
+                <LinkButton to="editar/1" className="buttons__button">
                     Editar
                 </LinkButton>
 
                 <LinkButton
-                    to="/remover/1"
+                    to="remover/1"
                     className="buttons__button buttons__button--danger"
                 >
                     Excluir
