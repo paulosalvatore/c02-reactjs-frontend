c24348edd70c47d88e98bfd5414bce8d32d90672
Paulo Salvatore <salvatoregames@gmail.com>
2021-09-10 15:44:39 -0300

    feat(personagem): add link do personagem card

--- src/components/PersonagensList/PersonagensList.js
+++ src/components/PersonagensList/PersonagensList.js
@@ -2,6 +2,7 @@ import PersonagensCard from "../PersonagensCard/PersonagensCard";
 import React from "react";
 import { useEffect, useState } from "react";
 import { Api } from "../../api/Api";
+import { Link } from "react-router-dom";
 
 export function PersonagensList() {
     const [personagens, setPersonagens] = useState([]);
@@ -19,13 +20,15 @@ export function PersonagensList() {
     }, []);
 
     return (
-        <div className="cards">
-            {personagens.map((personagem, index) => (
-                <PersonagensCard
-                    personagem={personagem}
-                    key={"personagem_" + index}
-                ></PersonagensCard>
-            ))}
-        </div>
+        <Link to="/personagem/1">
+            <div className="cards">
+                {personagens.map((personagem, index) => (
+                    <PersonagensCard
+                        personagem={personagem}
+                        key={"personagem_" + index}
+                    ></PersonagensCard>
+                ))}
+            </div>
+        </Link>
     );
 }
