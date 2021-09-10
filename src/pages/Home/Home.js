e9ce3c6a3f9ed1a2b1cc7ca0351c0b9c1ebb094a
Paulo Salvatore <salvatoregames@gmail.com>
2021-09-10 15:48:30 -0300

    feat(home): implement 'Adicionar' button

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,18 @@
+import LinkButton from "components/utils/LinkButton";
 import React from "react";
 import { PersonagensList } from "../../components/PersonagensList/PersonagensList";
 
 export function Home() {
     return (
-        <div>
+        <div className="home">
+            <div className="buttons">
+                <LinkButton
+                    to="personagem/adicionar"
+                    className="buttons__button buttons__button--success"
+                >
+                    Adicionar
+                </LinkButton>
+            </div>
             <PersonagensList></PersonagensList>
         </div>
     );
