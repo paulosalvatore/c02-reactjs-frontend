a8b9ef714e550b6c870b6391c18c7f4b72c2521b
Paulo Salvatore <salvatoregames@gmail.com>
2021-09-10 15:10:56 -0300

    feat(personagem): implement html elements - 'visualizar'

--- src/pages/VisualizarPersonagem/VisualizarPersonagem.js
+++ src/pages/VisualizarPersonagem/VisualizarPersonagem.js
@@ -1,5 +1,31 @@
 import React from "react";
 
 export default function VisualizarPersonagem() {
-    return <div>VisualizarPersonagem</div>;
+    return (
+        <div className="personagem">
+            <div className="personagem__buttons buttons">
+                <button className="buttons__button">Editar</button>
+                <button className="buttons__button buttons__button--danger">
+                    Excluir
+                </button>
+            </div>
+
+            <div className="personagem__details-wrapper">
+                <h1 className="personagem__title">Rick Sanchez</h1>
+
+                <div className="personagem__details">
+                    <img
+                        className="personagem__avatar"
+                        src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+                        alt="Rick Sanchez"
+                        title="Rick Sanchez"
+                    />
+
+                    <p className="personagem__description">
+                        Informações sobre o Rick Sanchez
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
